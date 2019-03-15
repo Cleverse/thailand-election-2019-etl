dbe5934efbe9dcc31bfbee3e565145a0fbb47cd3
Kohpai <srikote_n@cleverse.com>
2019-03-15 14:06:35 +0700

    aggregate province and party

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,15 @@ import { https } from 'firebase-functions'
 import { newFakeMapper } from './mapper/FakeMapper'
 import { IScore } from './mapper/IMapper'
 
-export const main = https.onRequest(async (_, res) => {
+import * as tempParties from './masterData/parties.json'
+import * as tempProvinces from './masterData/provinces.json'
+
+const partyData: any = tempParties
+const provinceData: any = tempProvinces
+
+export const main = https.onRequest(async (__, res) => {
     const mapper = newFakeMapper()
     const score = await mapper.score()
-    const parties = await mapper.parties()
 
     const zoneMap = score.reduce(
         (map, s) => {
@@ -21,62 +26,45 @@ export const main = https.onRequest(async (_, res) => {
         {} as any
     )
 
-    const partyMap = parties.reduce(
-        (map, p) => {
-            const partyId = `${p.id}`
-            map[partyId] = map[partyId] || []
-            map[partyId] = p
-            return map
-        },
-        {} as any
-    )
+    const provinces = Object.keys(zoneMap).map(id => {
+        const province = provinceData[id]
+        return {
+            provinceCode: province.code,
+            provinceName: province.name,
+            zones: Object.keys(zoneMap[id]).map(zoneNo => {
+                const items: IScore[] = zoneMap[id][zoneNo]
+                items.sort((a, b) => (a.score > b.score ? -1 : 1))
 
-    const result = Object.keys(zoneMap).reduce(
-        (provinces, id) => {
-            provinces.push({
-                provinceCode: id,
-                provinceName: '',
-                zones: Object.keys(zoneMap[id]).reduce(
-                    (zones, no) => {
-                        const items: IScore[] = zoneMap[id][no]
-                        items.sort((a, b) => (a.score > b.score ? -1 : 1))
-
-                        const candidates = items.slice(0, 3).map(item => {
-                            const party = item.partyId
-                                ? partyMap[item.partyId]
-                                : {
-                                      name: '???',
-                                      codeEN: '???',
-                                      logoUrl: '???',
-                                  }
-
-                            return {
-                                partyName: party.name,
-                                partyCode: party.codeEN,
-                                partyPic: party.logoUrl,
-                                candidate: `${item.title} ${item.firstName} ${
-                                    item.lastName
-                                }`,
-                                score: item.score,
-                                picture: '',
-                            }
-                        })
-                        zones.push({
-                            zoneNo: no,
-                            first3Candidates: candidates,
-                        })
-                        return zones
-                    },
-                    [] as any
-                ),
-            })
-            return provinces
-        },
-        [] as any
-    )
+                const candidates = items.slice(0, 3).map(mapCandidate)
+                return {
+                    zoneNo,
+                    first3Candidates: candidates,
+                }
+            }),
+        }
+    })
 
     res.status(200)
     res.type('application/json')
-    res.write(JSON.stringify(result))
+    res.write(JSON.stringify(provinces))
     res.end()
 })
+
+function mapCandidate(item: IScore) {
+    const party = item.partyId
+        ? partyData[item.partyId]
+        : {
+              name: '???',
+              codeEN: '???',
+              logoUrl: '???',
+          }
+
+    return {
+        partyName: party.name,
+        partyCode: party.codeEN,
+        partyPic: party.logoUrl,
+        candidate: `${item.title} ${item.firstName} ${item.lastName}`,
+        score: item.score,
+        picture: '',
+    }
+}
