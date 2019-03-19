68c200ec6886d01e1ade3e17deb31eed92864414
Kohpai <srikote_n@cleverse.com>
2019-03-19 20:45:05 +0700

    save to GCS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
 import { https } from 'firebase-functions'
+import { Storage } from '@google-cloud/storage'
 
 import { etlMapData } from './etl/map'
 import { etlPartylistData } from './etl/partylist'
 import { etlOverallData } from './etl/overall'
+
+const bucketName = 'thailand-election-2019.appspot.com'
+const storage = new Storage()
 
 export const main = https.onRequest(async (_, res) => {
     const [mapData, partylistData, overallData] = await Promise.all([
@@ -11,16 +15,28 @@ export const main = https.onRequest(async (_, res) => {
         etlOverallData(),
     ])
 
-    res.status(200)
-    res.type('application/json')
-    res.write(
-        JSON.stringify({
-            map: mapData,
-            partylist: partylistData,
-            overall: overallData,
-        })
-    )
-    res.end()
+    const now = Date.now()
+    const file = storage.bucket(bucketName).file(`data/${now}.json`)
+
+    const writeStream = file.createWriteStream({
+        contentType: 'application/json',
+        gzip: true,
+        resumable: false,
+    })
+    const jsonResponse = JSON.stringify({
+        map: mapData,
+        partylist: partylistData,
+        overall: overallData,
+        timestamp: Date.now,
+    })
+
+    await writeAsync(writeStream, jsonResponse)
+    writeStream.end(() => {
+        res.status(200)
+        res.type('application/json')
+        res.write(jsonResponse)
+        res.end()
+    })
 })
 
 export const map = https.onRequest(async (_, res) => {
@@ -49,3 +65,15 @@ export const overall = https.onRequest(async (_, res) => {
     res.write(JSON.stringify(overallData))
     res.end()
 })
+
+async function writeAsync(stream: NodeJS.WritableStream, json: string) {
+    return new Promise((resolve, reject) => {
+        stream.write(json, err => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
+    })
+}
