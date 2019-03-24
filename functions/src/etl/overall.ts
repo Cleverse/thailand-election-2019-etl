6352a5d31d69814303f810d66c4d1ac0cd14da79
Kohpai <srikote_n@cleverse.com>
2019-03-25 00:28:11 +0700

    fix overall

--- functions/src/etl/overall.ts
+++ functions/src/etl/overall.ts
@@ -1,4 +1,9 @@
-import { sortScores, calculateSeats, calculatePartyScoresMap } from './map'
+import {
+    sortScores,
+    calculateSeats,
+    calculatePartyScoresMap,
+    calculateSeatMap,
+} from './map'
 import { etlPartylistData } from './partylist'
 import { CDN_IMGURL } from '../constants'
 import { calculateTotalVotes, listToMap } from '../util'
@@ -8,7 +13,7 @@ import tempPartylist from '../masterData/partyToPartylistMembersMap.json'
 import tempProvinces from '../masterData/idToProvinceMap.json'
 import tempPartylistId from '../masterData/uniqueKeyToPartylistMemberMap.json'
 import tempConstituency from '../masterData/uniqueKeyToConstituencyMemberMap.json'
-import { newMapper } from '../mapper/IMapper'
+import { newMapper, IScore } from '../mapper/IMapper'
 
 const partyData: any = tempParties
 const provinceData: any = tempProvinces
@@ -18,34 +23,47 @@ const constituencyData: any = tempConstituency
 
 export async function etlOverallData() {
     const mapper = newMapper()
-    const scores = await mapper.scores()
-    const partySeats = calculateSeats(sortScores(scores))
+    const scoresByZone = await mapper.scores().then(sortScores)
+    const partySeatsMap = calculateSeatMap(scoresByZone)
+    const partyScoresMap = calculatePartyScoresMap(scoresByZone)
 
     const partylistMap = await etlPartylistData().then(list =>
         listToMap(list, 'partyName')
     )
 
-    return partySeats
-        .map(seats => {
-            const { codeEN, name, colorCode } = partyData[`${seats[0].partyId}`]
-            const partylist = partylistMap[name]
-            const constituencyCandidates = seats.map(seat => {
-                const { title, firstName, lastName, zone, provinceId } = seat
-                const { code, name: provinceName } = provinceData[
-                    `${provinceId}`
-                ]
-                const constituency =
-                    constituencyData[`${provinceName}:${zone}:${name}`]
-                const imgName = constituency
-                    ? `${constituency.guid}.jpg`
-                    : 'placeholder.png'
+    return Object.values(partyScoresMap)
+        .map((s: any) => {
+            const scores: IScore[] = s
+            const partyId = `${scores[0].partyId}`
+            const { codeEN, name, colorCode } = partyData[partyId]
+            const seats: IScore[] = partySeatsMap[partyId]
 
-                return {
-                    candidate: `${title} ${firstName} ${lastName}`,
-                    picture: `${CDN_IMGURL}/candidates/${imgName.toLowerCase()}`,
-                    zone: `${code}:${zone}`,
-                }
-            })
+            const partylist = partylistMap[name]
+            const constituencyCandidates = seats
+                ? seats.map(seat => {
+                      const {
+                          title,
+                          firstName,
+                          lastName,
+                          zone,
+                          provinceId,
+                      } = seat
+                      const { code, name: provinceName } = provinceData[
+                          `${provinceId}`
+                      ]
+                      const constituency =
+                          constituencyData[`${provinceName}:${zone}:${name}`]
+                      const imgName = constituency
+                          ? `${constituency.guid}.jpg`
+                          : 'placeholder.png'
+
+                      return {
+                          candidate: `${title} ${firstName} ${lastName}`,
+                          picture: `${CDN_IMGURL}/candidates/${imgName.toLowerCase()}`,
+                          zone: `${code}:${zone}`,
+                      }
+                  })
+                : []
 
             const constituencySeats = constituencyCandidates.length
             const partylistSeats = partylist ? partylist.seats : 0
@@ -75,6 +93,7 @@ export async function etlOverallData() {
                 partylistCandidates,
             }
         })
+        .filter(e => e.seats > 0)
         .sort((a, b) => b.seats - a.seats)
 }
 
